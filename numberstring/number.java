549e49bf56d030f45a3e337ce2793eb1a7af9e26
dnjsgud0625 <dnjsgud0625@naver.com>
2021-02-10 22:15:41 +0900

    number & string

---
+++ numberstring/number.java
@@ -0,0 +1,12 @@
+package numberstring;
+
+public class number {
+
+	public static void main(String[] args) {
+	System.out.println(1+3);
+	System.out.println(2.3+1.1);
+	System.out.println(3*4);
+	System.out.println(10/2);
+	}
+
+}
---
+++ numberstring/string.java
@@ -0,0 +1,12 @@
+package numberstring;
+
+public class string {
+
+	public static void main(String[] args) {
+		System.out.println("S"+"A");
+		System.out.println(1+1);
+		System.out.println("1+1");
+		System.out.println("He said \"Hello World\"");
+	}
+
+}
