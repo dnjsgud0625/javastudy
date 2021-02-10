efdecdab58a3650519d87d574ee90455da294e1d
dnjsgud0625 <dnjsgud0625@naver.com>
2021-02-10 22:35:29 +0900

    Variable

---
+++ Variable/doubletype.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class doubletype {
+
+	public static void main(String[] args) {
+		double b;
+		b = 3.14;
+		System.out.println(b);
+		System.out.println(b+1.23);
+		b = 3;
+		System.out.println(b);
+	}
+
+}
---
+++ Variable/stringtype.java
@@ -0,0 +1,13 @@
+package Variable;
+
+public class stringtype {
+
+	public static void main(String[] args) {
+		String a = "Hello";
+		String b = " World";
+		System.out.println(a);
+		System.out.println(a+b);
+		
+	}
+
+}
