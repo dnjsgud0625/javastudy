c035e8ca58084fe17b2c5583bc441ff195487c36
dnjsgud0625 <dnjsgud0625@naver.com>
2021-02-11 20:37:14 +0900

    condition

---
+++ condition/demo.java
@@ -0,0 +1,12 @@
+package condition;
+
+public class demo {
+
+	public static void main(String[] args) {
+		String id = args[0];
+		if(id.equals("egoing"))	System.out.println("right");
+		else	System.out.println("wrong");
+
+	}
+
+}
---
+++ condition/ifstatement.java
@@ -0,0 +1,9 @@
+package condition;
+
+public class ifstatement {
+
+	public static void main(String[] args) {
+		if(true)	System.out.println("result : true");
+		if(false)	System.out.println("result : false");
+	}
+}
