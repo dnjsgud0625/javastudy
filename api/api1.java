531765d5b4d1a961fad38bec0adf5579550f65b9
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-10 22:07:19 +0900

    api

---
+++ api/api1.java
@@ -0,0 +1,9 @@
+package api;
+
+public class api1 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+
+	}
+}
---
+++ api/api2.java
@@ -0,0 +1,9 @@
+package api;
+import java.lang.*;
+
+public class api2 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+}
