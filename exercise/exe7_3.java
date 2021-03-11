65af57aa71a5b0e51a4f874eef3b0b0e6c53b4a5
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-12 00:18:59 +0900

    exercise

---
+++ exercise/exe7_3.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.util.*;
+
+public class exe7_3 {
+	static int med(int a, int b, int c) {
+		int med = 0;
+		med = (a>=b)?((a>=c)?((b>=c)?b:c):a):((b>=c)?((a>=c)?a:c):b);
+		return med;
+	}
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		System.out.print("세 정수 입력 : ");
+		int a = num.nextInt();
+		int b = num.nextInt();
+		int c = num.nextInt();
+		num.close();
+		System.out.println("med : " + med(a, b, c));
+	}
+}
