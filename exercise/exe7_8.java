9f960f31414d3620f69ec2ca07eda1f4777723b7
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-13 23:34:20 +0900

    exercise

---
+++ exercise/exe7_8.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.util.*;
+
+public class exe7_8 {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		Random rand = new Random();
+		System.out.println("변수의 범위 입력");
+		System.out.print("a : ");
+		int a = num.nextInt();
+		System.out.print("b : ");
+		int b = num.nextInt();
+		num.close();
+		
+		int n = rand.nextInt(b-a+1)+a;
+		System.out.print("a이상 b미만의 난수 : "+n);
+	}
+}
