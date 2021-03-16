8afa5033d67c8fda59143b43270022498d458dbb
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-16 23:51:06 +0900

    exercise

---
+++ exercise/exe7_9.java
@@ -0,0 +1,23 @@
+package exercise;
+
+import java.util.*;
+
+public class exe7_9 {
+	static int readPlusInt(int n) {
+		return n;
+	}
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("양의 정숫값 : ");
+			int n = num.nextInt();
+			if(n<=0)	readPlusInt(n);
+			else {
+				readPlusInt(n);
+				break;
+			}
+		}
+		num.close();
+	}
+}
