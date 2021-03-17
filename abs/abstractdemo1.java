6dff6f9988fc662d32c0eefbf451596d2243d836
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-17 21:25:57 +0900

    abs

---
+++ abs/abstractdemo1.java
@@ -0,0 +1,20 @@
+package abs;
+abstract class A {
+	public abstract int b();
+	public void d() {
+		System.out.println("hi");
+	}
+}
+
+class B extends A{
+	public int b() {
+		return 1;
+	}
+}
+
+public class abstractdemo1 {
+
+	public static void main(String[] args) {
+		B obj = new B();
+	}
+}
