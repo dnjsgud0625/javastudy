a687cf3eb7a4a53810592e0dd7b2035cd5828b4f
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-21 23:36:02 +0900

    exercise2

---
+++ exercise2/exe2_01.java
@@ -0,0 +1,17 @@
+package exercise2;
+
+public class exe2_01 {
+
+	public static void main(String[] args) {
+		double x, y;
+
+		x = 63.33;
+		y = 3.14;
+
+		System.out.println("x의 값은 : " + x + "입니다.");
+		System.out.println("y의 값은 : " + y + "입니다.");
+
+		System.out.println("합계는 " + (x + y) + "입니다.");
+		System.out.println("평균은 " + (x + y) / 2 + "입니다.");
+	}
+}
