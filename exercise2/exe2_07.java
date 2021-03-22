9b64ce6dd77c96635aa6c1c3ecda4204827bd294
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-22 23:31:07 +0900

    exercise2

---
+++ exercise2/exe2_07.java
@@ -0,0 +1,16 @@
+package exercise2;
+
+import java.util.*;
+
+public class exe2_07 {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int x = rand.nextInt(9) + 1;
+		int y = rand.nextInt(9) - 9;
+		int z = rand.nextInt(90) + 10;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+}
