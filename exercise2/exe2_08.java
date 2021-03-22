4be581abc7622b20bd81fc031b8cab2932c46407
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-22 23:41:22 +0900

    exercise2

--- exercise2/exe2_08.java
+++ exercise2/exe2_08.java
@@ -9,6 +9,7 @@ public class exe2_08 {
 		Scanner stdIn = new Scanner(System.in);
 		System.out.print("정숫값 : ");
 		int x = stdIn.nextInt();
+		stdIn.close();
 		System.out.println("이 값의 +-5의 난수를 생성했습니다.");
 		int y = rand.nextInt(5) - 5;
 		System.out.print("생성한 난수는 " + (x + y) + "입니다.");
