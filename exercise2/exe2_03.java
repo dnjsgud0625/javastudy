c1f590d0a294a590e51d8c0390aa5791c24a2f4a
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-22 22:49:41 +0900

    exercise2

---
+++ exercise2/exe2_03.java
@@ -0,0 +1,14 @@
+package exercise2;
+
+import java.util.*;
+
+public class exe2_03 {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		System.out.print("정숫값 : ");
+		int x = num.nextInt();
+		num.close();
+		System.out.println(x + "이라고 입력했습니다.");
+	}
+}
