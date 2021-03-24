76314bd61d11dd9d8118d058edbcf3e9d20854ca
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-24 23:01:09 +0900

    exercise2

---
+++ exercise2/exe2_10.java
@@ -0,0 +1,17 @@
+package exercise2;
+
+import java.util.*;
+
+public class exe2_10 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("성 : ");
+		String a = stdIn.nextLine();
+		System.out.print("이름 : ");
+		String b = stdIn.nextLine();
+		System.out.println("안녕하세요 " + (a + b) + "씨!");
+		stdIn.close();
+	}
+
+}
