116b51f41b67297524825f4a7408392fc706717c
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-25 22:15:59 +0900

    exercise3

---
+++ exercise3/exe3_01.java
@@ -0,0 +1,17 @@
+package exercise3;
+
+import java.util.*;
+
+public class exe3_01 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("정숫값 : ");
+		int a = stdIn.nextInt();
+		if (a >= 0)
+			System.out.println("절댓값은 " + a + "입니다.");
+		else
+			System.out.println("절댓값은 " + (a * (-1)) + "입니다.");
+	}
+}
