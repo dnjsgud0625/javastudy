d9823342a4f76ed3c18a801cb0e8b2d2a5fd467b
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-30 23:21:20 +0900

    exercise4

---
+++ exercise4/exe4_04.java
@@ -0,0 +1,23 @@
+package exercise4;
+
+import java.util.*;
+
+public class exe4_04 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("카운트 시작합니다.");
+		int x;
+		do {
+			System.out.print("양의 정숫값 : ");
+			x = stdIn.nextInt();
+		} while (x <= 0);
+
+		while (x >= 0) {
+			System.out.println(x);
+			x--;
+		}
+		System.out.print(x);
+	}
+}
