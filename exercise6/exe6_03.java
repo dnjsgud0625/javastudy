82e26c3f24be1cc7e319b541f84f7c62899b716a
dnjsgud0625 <dnjsgud0625@naver.com>
2021-03-31 20:05:38 +0900

    exercise6

---
+++ exercise6/exe6_03.java
@@ -0,0 +1,12 @@
+package exercise6;
+
+public class exe6_03 {
+
+	public static void main(String[] args) {
+		double[] a = new double[5];
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.println((i + 1) + "." + (i + 1));
+		}
+	}
+}
